d7ebed70aadfb571b2aba4c070110fb83e630a37
fivanovic <fivanovic1999@gmail.com>
2021-03-31 11:06:28 +0200

    working dual serial distance measurement

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ try:
     RECEIVE4 = 15
     t1 = 0
     t2 = 0
-    duration = 0
+    duration1 = 0
+    duration2 = 0
+    duration3 = 0
+    duration4 = 0
     ss = 343
         
     GPIO.setup(TRIGGER, GPIO.OUT)
     GPIO.setup(RECEIVE1, GPIO.IN)
+    GPIO.setup(RECEIVE2, GPIO.IN)
         
     GPIO.output(TRIGGER, GPIO.LOW)
     print("Settling Sensor")
@@ -34,15 +38,36 @@ try:
             t1 = time.time()
         while GPIO.input(RECEIVE1)==1:
             t2 = time.time()
+                
+        duration1 = t2 - t1
+        #print("%f" % duration1)
+        dist1 = duration1*ss
+        #print("%f" % dist1)
+        time.sleep(0.5)
+        
+        GPIO.output(TRIGGER, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(TRIGGER, GPIO.LOW)
+        
+        while GPIO.input(RECEIVE2)==0:
+            t1 = time.time()
+        while GPIO.input(RECEIVE2)==1:
+            t2 = time.time()
             
-        duration = t2 - t1
-        print("%f" % duration)
-        dist = duration*ss
-        print("%f" % dist)
-        if(duration >= 0.038):
-            print("CLEAR")
+        duration2 = t2 - t1
+        #print("%f" % duration2)
+        dist2 = duration2*ss
+        #print("%f" % dist2)
+        
+        if(duration1 >= 0.038):
+            print("CLEAR STATION 1")
         else:
-            print("PING")
+            print("PING STATION 1 at %.2f" % dist1)
+            
+        if(duration2 >= 0.038):
+            print("CLEAR STATION 2")
+        else:
+            print("PING STATION 2 at %.2f" %dist2)
         time.sleep(2)
         
         
