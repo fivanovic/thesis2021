63b77a294eaac5c51280140456e88952d37cac27
fivanovic <fivanovic1999@gmail.com>
2021-04-01 16:51:27 +0200

    working basic flash identification server

--- server.py
+++ server.py
@@ -3,26 +3,35 @@ import time
 import threading
 
 HEADER = 64
-global FLASH
+
+FLASH = "0"
 FORMAT = 'utf-8'
 DISCONNECT_MSG = "Disconnected"
 CHECKER = "PING"
+CONCHECK = 0
+CONF = "CONF"
 ip = "192.168.1.233"
 #start server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((ip, 8080))
 
-def trigger():
+#this will be the function that controls the triggering of the sent signal
+def trig(conn, addr):
     global FLASH
-    FLASH = "1"
-    time.sleep(1)
-    FLASH = "0"
-    time.sleep(1)
+    while True:
+        FLASH = "0"
+        print(f"{FLASH}")
+        time.sleep(1)
+        FLASH = "1"
+        conn.send(FLASH.encode(FORMAT))
+        print(f"{FLASH}")
+        time.sleep(1)
 
-x = threading.Thread(target=trigger, args=())
 
 
+#this function will handle each receiver pi or the central receiver pi depending on architecture
 def handle_client(conn, addr):
+    global FLASH
     print(f"New Connection {addr} connected")
     connected = True
     while connected:
@@ -34,21 +43,29 @@ def handle_client(conn, addr):
             if msg == DISCONNECT_MSG:
                 connected = False
             print(f"{addr} {msg}")
-
-            if msg == CHECKER:
-                conn.send(FLASH.encode(FORMAT))
-        time.sleep(1)
+            
+            if msg == CONF:
+                CONCHECK = 1
+            
+            
+            
+        time.sleep(0.01)
         #conn.send("test".encode(FORMAT))
 
 
 def start():
+    
     server.listen()
     while True:
-        conn, addr = server.accept()
+        conn, addr = server.accept()      
         thread = threading.Thread(target=handle_client, args=(conn, addr))
+        x = threading.Thread(target=trig, args=(conn, addr))
+        x.start()
         thread.start()
         print(f"[Active Connections] {threading.activeCount() -2}")
+        
 print(f"SERVER: started on {ip}")
 
+
 start()
-x.start()
+
