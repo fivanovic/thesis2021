acb35c50070dbdf3ca32815790a6c874d39d0db3
fivanovic <fivanovic1999@gmail.com>
2021-04-02 14:07:06 +0200

    working sequential identification of stations, they must be connected in the order the user desires

--- server.py
+++ server.py
@@ -5,13 +5,18 @@ import threading
 HEADER = 64
 
 FLASH = "0"
-n = 0
+THREADS = []
 FORMAT = 'utf-8'
 DISCONNECT_MSG = "Disconnected"
 CHECKER = "PING"
 CONCHECK = 0
 CONF = "CONF"
 ip = "192.168.1.233"
+
+S1DIST = "0"
+S2DIST = "0"
+S3DIST = "0"
+S4DIST = "0"
 #start server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((ip, 8080))
@@ -33,21 +38,32 @@ def trig(conn, addr):
 #this function will handle each receiver pi or the central receiver pi depending on architecture
 def handle_client(conn, addr):
     global FLASH
+    global S1DIST
+    global S2DIST
+    global S3DIST
+    global S4DIST
+    global thread
     print(f"New Connection {addr} connected")
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
-            print(threading.getName())
+            print(thread.name)
             msg = conn.recv(msg_length).decode(FORMAT)
-
             if msg == DISCONNECT_MSG:
                 connected = False
+            if thread.name == "Thread-1":
+                S1DIST = msg
+            elif thread.name == "Thread-2":
+                S2DIST = msg
+            elif thread.name == "Thread-3":
+                S3DIST = msg
+            elif thread.name == "Thread-4":
+                S4DIST = msg
             print(f"{addr} {msg}")
 
-            if msg == CONF:
-                CONCHECK = 1
+            print(f"Station 1 distance is {S1DIST}")
 
 
 
@@ -56,15 +72,15 @@ def handle_client(conn, addr):
 
 
 def start():
-
+    global thread
     server.listen()
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr), name=n))
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         x = threading.Thread(target=trig, args=(conn, addr))
         x.start()
         thread.start()
-        n += 1
+        
         print(f"[Active Connections] {threading.activeCount() -2}")
 
 print(f"SERVER: started on {ip}")
