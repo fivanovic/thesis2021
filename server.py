ee59b86932894db3dba7bb88d832f81c59d2ae0f
fivanovic <fivanovic1999@gmail.com>
2021-04-13 16:28:59 +0200

    added math

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 import socket
 import time
 import threading
+import math
+import numpy as np
+import sympy as sym
+
+Station1 = np.array((100,100))
+Station2 = np.array((100,0))
+Station3 = np.array((0,0))
+Station4 = np.array((0,100))
 
 HEADER = 64
 
-StationNumber = 2 
+StationNumber = 4
 statnum = 1
 
 FLASH = "0"
@@ -27,8 +35,29 @@ server.bind((ip, 8080))
 #this will be the function that controls the triggering of the sent signal
 def trig(conn, addr):
     global FLASH
+    global S1DIST
+    global S2DIST
+    global S3DIST
+    global S4DIST
     while True:
         FLASH = "0"
+        radsq1 = S1DIST**2
+        radsq2 = S2DIST**2
+        radsq3 = S3DIST**2
+        radsq4 = S4DIST**2
+        
+        finrad1 = radsq1 - Station1[0]**2 - Station1[1]**2
+        finrad2 = radsq2 - Station2[0]**2 - Station2[1]**2
+        finrad3 = radsq3 - Station3[0]**2 - Station3[1]**2
+        finrad4 = radsq4 - Station4[0]**2 - Station4[1]**2
+        
+        x,y = sym.symbols('x,y')
+        eq1 = sym.Eq(x**2+y**2-(2*Station1[0])*x-(2*Station1[1])*y,finrad1)
+        eq2 = sym.Eq(x**2+y**2-(2*Station2[0])*x-(2*Station2[1])*y,finrad2)
+        eq3 = sym.Eq(x**2+y**2-(2*Station3[0])*x-(2*Station3[1])*y,finrad3)
+        eq4 = sym.Eq(x**2+y**2-(2*Station4[0])*x-(2*Station4[1])*y,finrad4)
+        result = sym.solve([eq1,eq2,eq3,eq4],(x,y))
+        print(result)
         #print(f"{FLASH}")
         time.sleep(1)
         FLASH = "1"
@@ -80,11 +109,16 @@ def handle_client(conn, addr):
 def start():
     global thread
     global statnum
+    global S1DIST
+    global S2DIST
+    global S3DIST
+    global S4DIST
     server.listen()
     while True:
         if (threading.activeCount() -1) == StationNumber:
             x = threading.Thread(target=trig, args=(conn, addr))
             x.start()
+            
         else:
             conn, addr = server.accept()
             thread = threading.Thread(target=handle_client, args=(conn, addr))
