2e3d306bde99e3041e1b0e1ae1a62a0fd0deb6bd
fivanovic <fivanovic1999@gmail.com>
2021-04-13 18:19:18 +0200

    cross your fucking fingers bois

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import threading
 import math
 import numpy as np
 import sympy as sym
+import localization as lx
+
 
 Station1 = np.array((100,100))
 Station2 = np.array((100,0))
@@ -51,33 +53,24 @@ def trig(conn, addr):
         time.sleep(1)
 
         FLASH = "0"
-        
-        radsq1 = float(S1DIST)**2
-        radsq2 = float(S2DIST)**2
-        radsq3 = float(S3DIST)**2
-        radsq4 = float(S4DIST)**2
-        
-        
-        finrad1 = radsq1 - Station1[0]**2 - Station1[1]**2
-        finrad2 = radsq2 - Station2[0]**2 - Station2[1]**2
-        finrad3 = radsq3 - Station3[0]**2 - Station3[1]**2
-        finrad4 = radsq4 - Station4[0]**2 - Station4[1]**2
-        print(finrad1)
-        print(finrad2)
-        print(finrad3)
-        print(finrad4)
-        
-        x,y = sym.symbols('x,y')
-        eq1 = sym.Eq(x**2+y**2-(2*Station1[0])*x-(2*Station1[1])*y,finrad1)
-        print(eq1)
-        eq2 = sym.Eq(x**2+y**2-(2*Station2[0])*x-(2*Station2[1])*y,finrad2)
-        eq3 = sym.Eq(x**2+y**2-(2*Station3[0])*x-(2*Station3[1])*y,finrad3)
-        eq4 = sym.Eq(x**2+y**2-(2*Station4[0])*x-(2*Station4[1])*y,finrad4)
-        loc = sym.solve([eq1,eq2,eq3,eq4],(x,y))
-        print(loc)
-        #print(f"{FLASH}")
+
+        P=lx.Project(mode='2D',solver='LSE')
+
+        P.add_anchor('Station1',(Station1[0],Station1[1]))
+        P.add_anchor('Station2',(Station2[0],Station2[1]))
+        P.add_anchor('Station3',(Station3[0],Station3[1]))
+        P.add_anchor('Station4',(Station4[0],Station4[1]))
+
+        device,label=P.add_target()
+
+        device.add_measure('Station1',S1DIST)
+        device.add_measure('Station2',S2DIST)
+        device.add_measure('Station3',S3DIST)
+        device.add_measure('Station4',S4DIST)
+        P.solve()
+        print(device.loc)
         time.sleep(1)
-        
+
 
 
 
@@ -131,7 +124,7 @@ def start():
         if (threading.activeCount() -1) == StationNumber:
             x = threading.Thread(target=trig, args=(conn, addr))
             x.start()
-            
+
         else:
             conn, addr = server.accept()
             thread = threading.Thread(target=handle_client, args=(conn, addr))
