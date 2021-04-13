0d3f46a401964606336a8f73f5e6017b2cf554b0
fivanovic <fivanovic1999@gmail.com>
2021-04-13 17:48:10 +0200

    work in prograss multilat

--- mathtest.py
+++ mathtest.py
@@ -1,18 +1,28 @@
 import math
 import numpy as np
 import sympy as sym
+from sympy.plotting import plot
+from sympy.plotting import plot_parametric
+import matplotlib
+import matplotlib.pyplot as plt 
 
 Station1 = np.array((100,100))
 Station2 = np.array((100,0))
 Station3 = np.array((0,0))
 Station4 = np.array((0,100))
+tester = 70.71
+device = np.array((50,50))
 
-device = np.array((32,50))
+#dist1 = np.linalg.norm(device-Station1)
+#dist2 = np.linalg.norm(device-Station2)
+#dist3 = np.linalg.norm(device-Station3)
+#dist4 = np.linalg.norm(device-Station4)
 
-dist1 = np.linalg.norm(device-Station1)
-dist2 = np.linalg.norm(device-Station2)
-dist3 = np.linalg.norm(device-Station3)
-dist4 = np.linalg.norm(device-Station4)
+dist1 = tester
+dist2 = tester
+dist3 = tester
+dist4 = tester
+
 
 radsq1 = dist1**2
 radsq2 = dist2**2
@@ -23,12 +33,24 @@ finrad1 = radsq1 - Station1[0]**2 - Station1[1]**2
 finrad2 = radsq2 - Station2[0]**2 - Station2[1]**2
 finrad3 = radsq3 - Station3[0]**2 - Station3[1]**2
 finrad4 = radsq4 - Station4[0]**2 - Station4[1]**2
+#print(finrad1)
+#print(finrad2)
+#print(finrad3)
+#print(finrad4)
 
-x,y = sym.symbols('x,y')
+x,y,t = sym.symbols('x,y,t')
 eq1 = sym.Eq(x**2+y**2-(2*Station1[0])*x-(2*Station1[1])*y,finrad1)
+print(eq1)
+#p1 = plot(eq1,(x,0,100),(y,0,100))
 eq2 = sym.Eq(x**2+y**2-(2*Station2[0])*x-(2*Station2[1])*y,finrad2)
 eq3 = sym.Eq(x**2+y**2-(2*Station3[0])*x-(2*Station3[1])*y,finrad3)
 eq4 = sym.Eq(x**2+y**2-(2*Station4[0])*x-(2*Station4[1])*y,finrad4)
+figure, axes = plt.subplots()
+circ1 = plt.Circle((Station1[0],Station1[1]),dist1)
+axes.set_aspect(1)
+axes.add_artist(circ1)
+plt.title('test')
+plt.show()
 result = sym.solve([eq1,eq2,eq3,eq4],(x,y))
 print(result)
 
--- server.py
+++ server.py
@@ -39,32 +39,45 @@ def trig(conn, addr):
     global S2DIST
     global S3DIST
     global S4DIST
+    global Station1
+    global Station2
+    global Station3
+    global Station4
     while True:
-        FLASH = "0"
-        radsq1 = S1DIST**2
-        radsq2 = S2DIST**2
-        radsq3 = S3DIST**2
-        radsq4 = S4DIST**2
-        
-        finrad1 = radsq1 - Station1[0]**2 - Station1[1]**2
-        finrad2 = radsq2 - Station2[0]**2 - Station2[1]**2
-        finrad3 = radsq3 - Station3[0]**2 - Station3[1]**2
-        finrad4 = radsq4 - Station4[0]**2 - Station4[1]**2
-        
-        x,y = sym.symbols('x,y')
-        eq1 = sym.Eq(x**2+y**2-(2*Station1[0])*x-(2*Station1[1])*y,finrad1)
-        eq2 = sym.Eq(x**2+y**2-(2*Station2[0])*x-(2*Station2[1])*y,finrad2)
-        eq3 = sym.Eq(x**2+y**2-(2*Station3[0])*x-(2*Station3[1])*y,finrad3)
-        eq4 = sym.Eq(x**2+y**2-(2*Station4[0])*x-(2*Station4[1])*y,finrad4)
-        result = sym.solve([eq1,eq2,eq3,eq4],(x,y))
-        print(result)
-        #print(f"{FLASH}")
-        time.sleep(1)
         FLASH = "1"
         for i in STATIONS:
             i.send(FLASH.encode(FORMAT))
         #print(f"{FLASH}")
         time.sleep(1)
+
+        FLASH = "0"
+        
+        radsq1 = float(S1DIST)**2
+        radsq2 = float(S2DIST)**2
+        radsq3 = float(S3DIST)**2
+        radsq4 = float(S4DIST)**2
+        
+        
+        finrad1 = radsq1 - Station1[0]**2 - Station1[1]**2
+        finrad2 = radsq2 - Station2[0]**2 - Station2[1]**2
+        finrad3 = radsq3 - Station3[0]**2 - Station3[1]**2
+        finrad4 = radsq4 - Station4[0]**2 - Station4[1]**2
+        print(finrad1)
+        print(finrad2)
+        print(finrad3)
+        print(finrad4)
+        
+        x,y = sym.symbols('x,y')
+        eq1 = sym.Eq(x**2+y**2-(2*Station1[0])*x-(2*Station1[1])*y,finrad1)
+        print(eq1)
+        eq2 = sym.Eq(x**2+y**2-(2*Station2[0])*x-(2*Station2[1])*y,finrad2)
+        eq3 = sym.Eq(x**2+y**2-(2*Station3[0])*x-(2*Station3[1])*y,finrad3)
+        eq4 = sym.Eq(x**2+y**2-(2*Station4[0])*x-(2*Station4[1])*y,finrad4)
+        loc = sym.solve([eq1,eq2,eq3,eq4],(x,y))
+        print(loc)
+        #print(f"{FLASH}")
+        time.sleep(1)
+        
 
 
 
@@ -82,20 +95,20 @@ def handle_client(conn, addr):
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
-            print(thread.name)
+            #print(thread.name)
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DISCONNECT_MSG:
                 connected = False
             if msg[:2] == "s1":
                 S1DIST = msg[3:]
-                print(f"Station 1 distance is {S1DIST}")
+                #print(f"Station 1 distance is {S1DIST}")
             elif msg[:2] == "s2":
                 S2DIST = msg[3:]
-                print(f"Station 2 distance is {S2DIST}")
-            elif thread.name == "Thread-3":
-                S3DIST = msg
-            elif thread.name == "Thread-4":
-                S4DIST = msg
+                #print(f"Station 2 distance is {S2DIST}")
+            elif msg[:2] == "s3":
+                S3DIST = msg[3:]
+            elif msg[:2] == "s4":
+                S4DIST = msg[3:]
             #print(f"{addr} {msg}")
 
             #print(f"Station 1 distance is {S1DIST}")
@@ -122,9 +135,9 @@ def start():
         else:
             conn, addr = server.accept()
             thread = threading.Thread(target=handle_client, args=(conn, addr))
-            thread.name = "Station" + str(statnum)
-            statnum+=1
-            print(f"next station will be {statnum}")
+            #thread.name = "Station" + str(statnum)
+            #statnum+=1
+            #print(f"next station will be {statnum}")
             STATIONS.append(conn)
             #print(thread.name)
             thread.start()
