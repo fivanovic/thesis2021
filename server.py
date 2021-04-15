567a6c5656de345bec80c83b8bfcf423817ca384
fivanovic <fivanovic1999@gmail.com>
2021-04-15 16:34:00 +0200

    sleeper

--- server.py
+++ server.py
@@ -68,16 +68,16 @@ try:
             #for i in STATIONS:
                 #i.send(FLASH.encode(FORMAT))
             #STATIONS[0].send(FLASH.encode(FORMAT))
-
+            for i in STATIONS:
+                i.send(FLASH.encode(FORMAT))
+            time.sleep(1)
             GPIO.output(TRIGGER, GPIO.HIGH)
             #STATIONS[0].send(FLASH.encode(FORMAT))
 
             #STATIONS[0].send(FLASH.encode(FORMAT))
             time.sleep(0.00001)
             GPIO.output(TRIGGER, GPIO.LOW)
-            time.sleep(0.01)
-            for i in STATIONS:
-                i.send(FLASH.encode(FORMAT))
+
             #print(f"{FLASH}")
             time.sleep(1)
             FLASH = "0"
