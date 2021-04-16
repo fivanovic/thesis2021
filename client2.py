6c8d6c6d0090513a21630dd53d5864db79af12b1
fivanovic <fivanovic1999@gmail.com>
2021-04-16 11:03:34 +0200

    flash as int

--- client2.py
+++ client2.py
@@ -40,7 +40,7 @@ try:
 
     ##send("Hello world!")
     while True:
-        resp = (client.recv(2048).decode(FORMAT))
+        resp = (client.recv(2048))
 
         GPIO.output(TRIGGER, GPIO.HIGH)
         time.sleep(0.00001)
