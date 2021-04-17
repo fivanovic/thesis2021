711ca3b9ae78f2b3e3bda88336c095202595b30b
fivanovic <fivanovic1999@gmail.com>
2021-04-17 11:26:39 +0200

    test3

--- servertester.py
+++ servertester.py
@@ -72,7 +72,7 @@ try:
         duration = rectime - sendtime
         #print("duration is %f" % duration )
         dist = duration*ss
-        print("distance is %f" % float(msg))
+        print("distance is %.2f" % float(msg))
         #print("%f time taken" % duration)
         time.sleep(1)
         #print("%f distance" % dist)
