fbf44a4a48f6b38d89933c2bbba52505bb9821fc
fivanovic <fivanovic1999@gmail.com>
2021-04-24 17:15:31 +0200

    local updated for 7 m and testing for undershooting

--- local.py
+++ local.py
@@ -10,16 +10,16 @@ import localization as lx
 P=lx.Project(mode='2D',solver='LSE')
 
 P.add_anchor('Station1',(0,0))
-P.add_anchor('Station2',(100,0))
-P.add_anchor('Station3',(0,100))
-P.add_anchor('Station4',(100,100))
+P.add_anchor('Station2',(7,0))
+P.add_anchor('Station3',(0,7))
+P.add_anchor('Station4',(7,7))
 
 device,label=P.add_target()
 
-device.add_measure('Station1',70.71)
-device.add_measure('Station2',70.71)
-device.add_measure('Station3',70.71)
-device.add_measure('Station4',70.71)
+device.add_measure('Station1',5.79)
+device.add_measure('Station2',6.36)
+device.add_measure('Station3',3.57)
+device.add_measure('Station4',4.44)
 
 P.solve()
 print(device.loc)
